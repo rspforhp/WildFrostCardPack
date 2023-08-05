54bf6cafe5d40b6522c45a47e961a8162dcc7917
Kopie <39800618+rspforhp@users.noreply.github.com>
2023-08-05 14:51:05 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ public class WildFrostCardPackMod : BasePlugin
            CardAdder.CreateCardData("ExampleMod", "ExampleCard").SetTitle("Example card").SetStats(1, 1, 3).SetIsUnit().SetCanPlay(CardAdder.CanPlay.CanPlayOnBoard)
                .SetAttackEffects(CardAdder.VanillaStatusEffects.Demonize.StatusEffectStack(1)).RegisterCardInApi();
        };
-       /// QWQ idk why but this way it crashes sometimes idk whats wrong with that, but usually it shouldnt crash i believe
        StatusEffectData data=null;
        StatusEffectAdder.OnAskForAddingStatusEffects+= delegate(int i)
        {
@@ -31,7 +30,6 @@ public class WildFrostCardPackMod : BasePlugin
        {
            // OR  CardAdder.CreateCardData("ExampleMod", "ExampleCardWithEffectGotBeforeRegistration").SetStats(1, 1, 3)
            //                    .SetAttackEffects( "ExampleMod.ExampleEffect". StatusEffectStack(1)).RegisterCardInApi();
-           // BUT it might fail, so this way its safer to get the effects u made yourself :3
            CardAdder.CreateCardData("ExampleMod", "ExampleCardWithEffectGotBeforeRegistration").SetTitle("Example Card 2").SetIsUnit().SetCanPlay(CardAdder.CanPlay.CanPlayOnBoard).SetStats(1, 1, 3)
                .SetStartWithEffects(new CardData.StatusEffectStacks(data,1)).RegisterCardInApi();
        };
